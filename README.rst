496ac1145ff78ec687a8fa96b0743901a69e5b83
Nathaniel Rodriguez <njrodrig@indiana.edu>
2016-08-21 14:55:08 -0400

    first commit

---
+++ README.rst
@@ -0,0 +1,8 @@
+To install nest goto:
+http://www.nest-simulator.org/download/
+
+and download 2.10.0+ 
+follow installation instructions on their site
+use --prefix for local install.
+Make sure it is installed with the same python exe
+that will be calling it.
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='neuralnetsim',
+    version='0.1',
+    description='Uses nest for simulating neural networks: Must have nest v2.10.0 or greater',
+    author='Nathaniel Rodriguez',
+    packages=['neuralnetsim'],
+    url='https://github.com/Nathaniel-Rodriguez/neuralnetsim.git',
+    install_requires=[
+          'networkx',
+          'numpy',
+          'matplotlib'
+      ],
+    dependency_links=['https://github.com/Nathaniel-Rodriguez/utilities.git'],
+    include_package_data=True,
+    zip_safe=False)
