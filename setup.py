ad0c391ccb62a2ddcd57c7719c0e692b24dea5e2
Nathaniel Rodriguez <njrodrig@indiana.edu>
2016-08-21 16:16:18 -0400

    dependency test

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setup(name='neuralnetsim',
           'matplotlib',
           'utilities'
       ],
-    dependency_links=['https://github.com/Nathaniel-Rodriguez/utilities.git@0.1#egg=utilities'],
+    dependency_links=['https://github.com/Nathaniel-Rodriguez/utilities.git@0.1'],
     include_package_data=True,
     zip_safe=False)
