e2e83132444466659c9d2c20c097cbd6f646fc6e
Nathaniel Rodriguez <njrodrig@indiana.edu>
2016-08-21 15:53:40 -0400

    dependency test

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setup(name='neuralnetsim',
           'matplotlib',
           'utilities>=0.1'
       ],
-    dependency_links=['https://github.com/Nathaniel-Rodriguez/utilities/archive/0.1.tar.gz#egg=utilities'],
+    dependency_links=['https://github.com/Nathaniel-Rodriguez/utilities/archive/0.1.tar.gz#egg=utilities-0.1'],
     include_package_data=True,
     zip_safe=False)
