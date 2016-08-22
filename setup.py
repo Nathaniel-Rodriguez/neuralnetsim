b0ab8d70f133de3e8c45b074118286ec9be1704d
Nathaniel Rodriguez <njrodrig@indiana.edu>
2016-08-21 22:06:24 -0400

    dependency complete

--- setup.py
+++ setup.py
@@ -14,8 +14,6 @@ setup(name='neuralnetsim',
           'networkx',
           'numpy',
           'matplotlib',
-          'utilities-0.1'
       ],
-    dependency_links=['https://github.com/Nathaniel-Rodriguez/utilities/archive/0.1.tar.gz@0.1#egg=utilities-0.1'],
     include_package_data=True,
     zip_safe=False)
