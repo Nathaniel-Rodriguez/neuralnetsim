f2a678a501840e57bc2d2e6685e9a707c5390c61
nathaniel-rodriguez <nathaniel.jm.rodriguez@gmail.com>
2020-10-11 18:45:07 -0500

    updated readme, gitignore, manifest, and reqs

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup
 
+
 def readme():
     with open('README.rst') as f:
         return f.read()
 
+
 setup(name='neuralnetsim',
-    version='0.1',
-    description='Uses nest for simulating neural networks: Must have nest v2.10.0 or greater',
-    author='Nathaniel Rodriguez',
-    packages=['neuralnetsim'],
-    url='https://github.com/Nathaniel-Rodriguez/neuralnetsim.git',
-    install_requires=[
-          'networkx',
-          'numpy',
-          'matplotlib',
+      version='0.1.0',
+      description='Library for the rodent neural slice project.',
+      author='Nathaniel Rodriguez',
+      packages=['neuralnetsim'],
+      url='https://github.com/Nathaniel-Rodriguez/neuralnetsim.git',
+      install_requires=[
+          'numpy'
       ],
-    include_package_data=True,
-    zip_safe=False)
+      include_package_data=True,
+      zip_safe=False)
