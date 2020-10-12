9a10bd10241fe8067c4f5174083bb82cbc30bad8
nathaniel-rodriguez <nathaniel.jm.rodriguez@gmail.com>
2020-10-11 21:52:06 -0500

    fixed tests for discovery

--- tests/test_data_loader.py
+++ tests/test_data_loader.py
@@ -1,14 +1,16 @@
-import neuralnetsim
-from pkg_resources import resource_filename
 import unittest
+import neuralnetsim
+from pkg_resources import resource_exists
+from pkg_resources import resource_filename
 
 
 class TestDataLoader(unittest.TestCase):
     def test_load_matrix(self):
-        file_path = resource_filename("neuralnetsim", "tests/test_data/pdf.mat")
+        self.assertTrue(resource_exists("tests", "test_data/pdf.mat"))
+        file_path = resource_filename("tests", "test_data/pdf.mat")
         mat = neuralnetsim.load_as_matrix(file_path, "pdf")
-        self.assertEqual(mat.shape[0], 384)
-        self.assertEqual(mat.shape[1], 384)
+        self.assertEqual(mat.shape[0], 243)
+        self.assertEqual(mat.shape[1], 243)
 
 
 if __name__ == '__main__':
