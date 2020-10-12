384aecbaceefb71d4d481d685e0ce93b39e9e496
nathaniel-rodriguez <nathaniel.jm.rodriguez@gmail.com>
2020-10-12 00:43:25 -0500

    added community detection and test

--- src/neuralnetsim/community_detection.py
+++ src/neuralnetsim/community_detection.py
@@ -20,19 +20,26 @@ def get_network(weight_matrix: np.ndarray,
     return nx.from_numpy_matrix(adj_matrix, create_using=nx.DiGraph)
 
 
-def add_communities(graph: nx.DiGraph) -> nx.DiGraph:
+def add_communities(graph: nx.DiGraph, seed=None,
+                    infomap_commands=None) -> nx.DiGraph:
     """
     Uses Infomap to detect communities in a given graph and then assigns
-    those communities as an attribute to the nodes of graph called "community".
+    those communities as an attribute to the nodes of graph called "level1"
+    for first depth level modules and "level2" for second depth level modules.
     :param graph: A weighted-directed networkx graph.
+    :param seed: A seed for Infomap (default: None).
+    :param infomap_commands: Optional command arguments for Infomap (default:
+        ["-N 10", "--two-level", "--directed", "--silent"]).
     :return: Updates graph in-place with community assignments, returns the
              provided graph.
     """
-    imap = infomap.Infomap("-N 10 --two-level --directed")
+    if infomap_commands is None:
+        infomap_commands = ["-N 10", "--two-level", "--directed", "--silent"]
+    if seed is not None:
+        infomap_commands.append("--seed " + str(seed))
+    imap = infomap.Infomap(" ".join(infomap_commands))
     imap.add_links(tuple((i, j, w) for i, j, w in graph.edges.data('weight')))
     imap.run()
-    nx.set_node_attributes(graph,
-                           {node.node_id: node.module_id
-                            for node in imap.tree if node.is_leaf},
-                           "community")
+    nx.set_node_attributes(graph, imap.get_modules(depth_level=1), "level1")
+    nx.set_node_attributes(graph, imap.get_modules(depth_level=2), "level2")
     return graph
--- tests/test_community_detection.py
+++ tests/test_community_detection.py
@@ -17,3 +17,9 @@ class TestCommunityDetection(unittest.TestCase):
         graph = neuralnetsim.get_network(self.weight_mat, self.link_mat)
         self.assertEqual(len(graph.nodes), 243)
         self.assertEqual(len(graph.edges), 1098)
+
+    def test_add_communities(self):
+        graph = neuralnetsim.get_network(self.weight_mat, self.link_mat)
+        neuralnetsim.add_communities(graph, seed=4953)
+        self.assertEqual(len({m for i, m in graph.nodes.data("level1")}), 20)
+        self.assertEqual(len({m for i, m in graph.nodes.data("level2")}), 2)
