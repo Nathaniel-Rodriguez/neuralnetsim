c553cee7c8e3dd33678bcc746df7141df74e3c2f
nathaniel-rodriguez <nathaniel.jm.rodriguez@gmail.com>
2020-10-11 21:51:44 -0500

    adjusted package layout

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 def readme():
@@ -10,15 +10,13 @@ setup(name='neuralnetsim',
       version='0.1.0',
       description='Library for the rodent neural slice project.',
       author='Nathaniel Rodriguez',
-      packages=['neuralnetsim'],
+      packages=find_packages('src'),
+      package_dir={'': 'src'},
       url='https://github.com/Nathaniel-Rodriguez/neuralnetsim.git',
       install_requires=[
           'numpy',
           'infomap>=1.1.4',
           'scipy'
       ],
-      package_data={
-          'neuralnetsim': ['tests/test_data/*']
-      },
-      include_package_data=True,
+      include_package_data=False,
       zip_safe=False)
