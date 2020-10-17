cdfc77a6ae506e8135f4de4b9b0183d406be9b23
nathaniel-rodriguez <nathaniel.jm.rodriguez@gmail.com>
2020-10-17 17:37:20 -0500

    added cooling schedules

---
+++ tests/test_cooling.py
@@ -0,0 +1,23 @@
+import unittest
+import neuralnetsim
+
+
+class TestExponentialSchedule(unittest.TestCase):
+    def test_initial_t(self):
+        pass
+
+    def test_step(self):
+        pass
+
+    def test_test_final_t(self):
+        pass
+
+    def test_start(self):
+        pass
+
+    def test_stop(self):
+        pass
+
+
+class TestAdaptiveSchedule(unittest.TestCase):
+    pass
