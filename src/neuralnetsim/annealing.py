9f3a885410566bc7c0abccf38d119af2d1b04c77
nathaniel-rodriguez <nathaniel.jm.rodriguez@gmail.com>
2020-10-19 00:18:32 -0500

    completed annealer

--- src/neuralnetsim/annealing.py
+++ src/neuralnetsim/annealing.py
@@ -1,73 +1,130 @@
-__all__ = []
+__all__ = ["NetworkAnnealer"]
 
 
 import numpy as np
 import networkx as nx
 import random
+import math
 from typing import Callable
+from typing import Tuple
+from typing import List
 from neuralnetsim import CoolingSchedule
+from collections import namedtuple
 
 
-def create_bridge_mask(graph: nx.DiGraph, key: str) -> np.ndarray:
-    """
-    Creates a numpy mask which is true for connections that are bridges.
-    :param graph: A networkx graph.
-    :param key: A key specifying the community node attribute.
-    :return: A numpy array of size NxN, where N is the number of nodes in the
-            graph.
-    """
-    # Construct matrix of 1s for bridge 0s for com
-    bridge_matrix = np.zeros((len(graph), len(graph)))
-    for node_i in graph.nodes:
-        for node_j in graph.nodes:
-            # check if edge is within or between communities
-            if graph.nodes[node_i][key] == graph.nodes[node_j][key]:
-                bridge_matrix[node_i, node_j] = 0
-            else:
-                bridge_matrix[node_i, node_j] = 1
-
-    return bridge_matrix != 0
+SwapResult = namedtuple("SwapResult", ["energy", "source_index", "source_edge",
+                                       "destination_edge", "destination_exists"])
 
 
-def create_log_matrix(adj_matrix: np.ndarray) -> np.ndarray:
-    """
-    Creates a copy of the adjacency matrix with log applied element-wise.
-    Zero entries are excluded.
-    :param adj_matrix: An adjacency matrix.
-    :return: The logged matrix copy.
-    """
-    log_matrix = adj_matrix.copy()
-    non_zero_mask = adj_matrix != 0
-    log_matrix[non_zero_mask] = np.log(adj_matrix[non_zero_mask])
-    return log_matrix
+def edge_swap(matrix: np.ndarray,
+              edges: List[Tuple[int, int]],
+              source_index: int,
+              source_edge: Tuple[int, int],
+              destination_edge: Tuple[int, int],
+              destination_exists: bool) -> np.ndarray:
+    # swap elements in matrix
+    matrix[source_edge[0], source_edge[1]], \
+    matrix[destination_edge[0], destination_edge[1]] = \
+        matrix[destination_edge[0], destination_edge[1]], \
+        matrix[source_edge[0], source_edge[1]]
+
+    # Create new edge if destination doesn't exist
+    if not destination_exists:
+        edges[source_index] = (destination_edge[0], destination_edge[1])
+
+    return matrix
 
 
 class NetworkAnnealer:
+    """
+    Generates a new network from a given one by minimizing a given energy
+    function.
+    """
     def __init__(self,
                  num_steps: int,
                  num_scramble_steps: int,
                  cooling_schedule: CoolingSchedule,
                  energy_function: Callable[[np.ndarray], float],
                  seed: int = None):
+        """
+        :param num_steps: The number of steps for the annealing process.
+        :param num_scramble_steps: The number of steps that will be spent
+        scrambling the network before annealing begins.
+        :param cooling_schedule: A cooling schedule for the annealing process.
+        The cooling schedule only activates following the scrambling period.
+        :param energy_function: An energy function that can be evaluated
+        when provided an adjacency matrix of the current network.
+        :param seed: A seed for random number generation.
+        """
         if seed is None:
             self._rng = random.Random()
         else:
             self._rng = random.Random(seed)
-        self._num_steps = num_steps
-        self._num_scramble_steps = num_scramble_steps
+        self.num_steps = num_steps
+        self.num_scramble_steps = num_scramble_steps
+        self._cooling_schedule = cooling_schedule
+        self._energy_function = energy_function
+        self._energy = None
+        self._history = []
+        self._acceptance_history = []
+        self._graph = None
 
-    def _swap(self):
-        pass
+    def _scramble(self, num_edges, num_nodes, adj_mat, edges):
+        # Pick random edge to move
+        source_index = self._rng.randrange(num_edges)
+        source_edge = edges[source_index]
+        # Pick random destination (prevent self-loops)
+        destination_edge = (self._rng.randrange(num_nodes),
+                            self._rng.randrange(num_nodes))
+        # keep redrawing if a self-loop is drawn
+        while destination_edge[0] == destination_edge[1]:
+            destination_edge = (self._rng.randrange(num_nodes),
+                                self._rng.randrange(num_nodes))
+        destination_exists = destination_edge in edges
+        energy = self._energy_function(
+            edge_swap(adj_mat, edges, source_index, source_edge,
+                      destination_edge, destination_exists))
 
-    def _step(self):
-        pass
+        return SwapResult(energy, source_index, source_edge,
+                          destination_edge, destination_exists)
 
-    def fit(self, graph: nx.DiGraph, community_key: str, **parameters):
-        num_edges = graph.number_of_edges()
-        num_nodes = graph.number_of_nodes()
-        adj_mat = nx.to_numpy_matrix(graph).getA()
-        bridge_mask = create_bridge_mask(graph, community_key)
-        log_mat = create_log_matrix(adj_mat)
+    def _anneal(self, adj_mat, edges, energy, source_index, source_edge,
+                destination_edge, destination_exists):
+        energy_diff = energy - self._energy
+        if energy_diff < 0:
+            self._acceptance_history.append(1)
+            accepted_energy = energy
+        else:
+            if self._rng.random() < math.exp(-energy_diff / self._cooling_schedule.t):
+                accepted_energy = energy
+                self._acceptance_history.append(1)
+            else:
+                self._acceptance_history.append(0)
+                edge_swap(adj_mat, edges, source_index,
+                          destination_edge, source_edge, destination_exists)
+                accepted_energy = self._energy
+        self._cooling_schedule.step(accepted_energy)
+        return accepted_energy
+
+    def fit(self, graph: nx.DiGraph):
+        self._graph = graph
+        num_edges = self._graph.number_of_edges()
+        num_nodes = self._graph.number_of_nodes()
+        adj_mat = nx.to_numpy_array(self._graph)
+        edges = np.argwhere(adj_mat).tolist()
+        self._energy = self._energy_function(adj_mat)
+        self._history.append(self._energy)
+
+        for i in range(self.num_scramble_steps):
+            self._energy = self._scramble(num_edges, num_nodes,
+                                          adj_mat, edges).energy
+            self._history.append(self._energy)
+
+        for i in range(self.num_steps):
+            self._energy = self._anneal(
+                adj_mat, edges, *self._scramble(
+                    num_edges, num_nodes, adj_mat, edges))
+            self._history.append(self._energy)
 
         return self
 
@@ -76,3 +133,35 @@ class NetworkAnnealer:
 
     def fit_predict(self, graph: nx.DiGraph) -> nx.DiGraph:
         return self.fit(graph).predict()
+
+    @property
+    def cooling_scheduler(self):
+        return self._cooling_schedule
+
+    @cooling_scheduler.setter
+    def cooling_scheduler(self, value):
+        raise NotImplementedError
+
+    @property
+    def energy_function(self):
+        return self._energy_function
+
+    @energy_function.setter
+    def energy_function(self, value):
+        raise NotImplementedError
+
+    @property
+    def energy_history(self):
+        return self._history
+
+    @energy_history.setter
+    def energy_history(self, v):
+        raise NotImplementedError
+
+    @property
+    def acceptance_history(self):
+        return self._acceptance_history
+
+    @acceptance_history.setter
+    def acceptance_history(self, v):
+        raise NotImplementedError
