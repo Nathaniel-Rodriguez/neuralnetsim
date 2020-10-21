641a114f0ce47f5b874f125c75b25d806dc0af25
nathaniel-rodriguez <nathaniel.jm.rodriguez@gmail.com>
2020-10-20 21:19:34 -0500

    added comments

--- src/neuralnetsim/data_loader.py
+++ src/neuralnetsim/data_loader.py
@@ -9,6 +9,9 @@ from pathlib import Path
 def load_as_matrix(file_path: Path, key: str) -> np.ndarray:
     """
     Opens and loads a matrix from a matlab file when given the data key.
+    Popular keys include 'data' for spike-trains, 'weights' for TE matrix,
+    'pdf' for binary significance matrix, 'x' for x-positions, 'y' for
+    y-positions.
     :param file_path: Path to a file containing a matrix.
     :param key: Key for the matrix.
     :return: A an array of the data.
