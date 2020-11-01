80384d0eb8390cd09f1f13ff1f815816b6003e97
nathaniel-rodriguez <nathaniel.jm.rodriguez@gmail.com>
2020-11-01 01:38:13 -0500

    added noise test

--- tests/test_optimizer_net.py
+++ tests/test_optimizer_net.py
@@ -9,7 +9,7 @@ class TestSubNetwork(unittest.TestCase):
     def setUp(self):
         nest.SetKernelStatus({'grng_seed': 984,
                               'rng_seeds': [684]})
-        nest.set_verbosity(0)
+        nest.set_verbosity(40)
         graph = nx.DiGraph()
         graph.add_edge(2, 1, weight=10000.0)
         graph.add_edge(3, 1, weight=10000.0)
@@ -33,3 +33,11 @@ class TestSubNetwork(unittest.TestCase):
         nest.Simulate(50.0)
         self.assertEqual(len(self.net.get_spike_output()), 1)
         self.assertAlmostEqual(self.net.get_spike_output()[0], 12.7, 2)
+
+    def test_noise(self):
+        nest.SetStatus(self.net._noise, {"mean": 1000.0})
+        nest.Simulate(50.0)
+        outputs = [5.9, 12.6, 19.3, 26.1, 32.9, 39.7, 46.4]
+        for i, spike_time in enumerate(outputs):
+            self.assertAlmostEqual(self.net.get_spike_output()[i],
+                                   spike_time, 2)
