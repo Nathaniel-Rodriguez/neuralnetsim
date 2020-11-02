a282abe7912416b52fd65299c506237528ae4a51
nathaniel-rodriguez <nathaniel.jm.rodriguez@gmail.com>
2020-11-02 11:31:59 -0600

    added ArrayTranslator

--- src/neuralnetsim/translators.py
+++ src/neuralnetsim/translators.py
@@ -4,10 +4,11 @@ __all__ = ["ValueTranslator", "ArrayTranslator",
 
 import math
 import networkx as nx
+import numpy as np
 from typing import List
-from typing import Set
-from typing import Callable
 from typing import Dict
+from typing import Any
+from typing import Set
 
 
 class ValueTranslator:
@@ -69,89 +70,117 @@ def get_translator(translators: List[ValueTranslator],
     raise KeyError("Failed to find translator in list with key: " + key)
 
 
-class CompositeTranslator:
-    """
-    The CompositeTranslator allows for mapping multiple optimizer
-    parameters to a single model parameter. This is desirable when some
-    model parameters are coupled and it is easier to optimize over some
-    composite parameter. For instance, voltage reset values can not exceed
-    voltage threshold values, so instead of trying to couple the two parameters
-    in some special case of the optimizer, it is easier to simply optimize
-    over a value range and then translate the range back into the distinct
-    voltage parameters.
-    """
-    def __init__(self, model_key: str, translator_list: List[ValueTranslator],
-                 to_model_function: Callable[[...], float],
-                 to_optimizer_function: Callable[[...], Dict]):
-        """
-        :param model_key: Key associated with the model parameter.
-        :param translator_list: A list of optimizers to use for
-        :param to_model_function: A function that takes keyword arguments for
-        each optimizer parameter value required.
-        :param to_optimizer_function: A function that takes a model parameter
-        value associated with the model_key and a set of key-value pairs of
-        other model parameter values and returns the associated
-        non-composite parameter keys and values in a dictionary. This function
-        is effectively the inverse of the to_optimizer_function. All model
-        keywords will be dumped into this function.
-        """
-        self.model_key = model_key
-        self._translator_list = translator_list
-        self._translator_keys = set([translator.key
-                                     for translator in self._translator_list])
-        self._to_model_function = to_model_function
-        self._to_optimizer_function = to_optimizer_function
-
-    @property
-    def translator_keys(self):
-        """
-        :return: Keys associated with all of the translators needed to
-        contruct the composite model parameter.
-        """
-        return self._translator_keys
-
-    @translator_keys.setter
-    def translator_keys(self, v):
-        raise NotImplementedError
-
-    def to_model(self, **optimizer_kwargs) -> float:
-        """
-        Converts an optimizer value into a model value.
-        :param optimizer_kwargs: key value pairs associated with each optimizer
-        value that the translator needs. The keys property returns a set of
-        key words that need to be filled. Raises a KeyError if not
-        all key words are found or too many are given.
-        :return: The converted value for the model.
-        """
-        if set(optimizer_kwargs.keys()) != self._translator_keys:
-            raise KeyError("Key mismatch between required translator keys"
-                           " and those provided. \nRequired: "
-                           + str(self._translator_keys) + "\nFound: "
-                           + str(set(optimizer_kwargs.keys())))
-        function_kwargs = {key: get_translator(self._translator_list,
-                                               key).to_model(value)
-                           for key, value in optimizer_kwargs.items()}
-        return self._to_model_function(**function_kwargs)
-
-    def to_optimizer(self, **model_kwargs) -> Dict:
-        """
-        Converts a model value into a optimizer values.
-        :param model_kwargs: Other model parameters that need to be used to
-        determine the composite optimizer parameter value. Kwargs are not
-        enforced. Method may use any or none of the provided keyword arguments.
-        :return: The converted key value pairs for the optimizer.
-        """
-        return {key: get_translator(self._translator_list, key).to_optimizer(value)
-                for key, value in self._to_optimizer_function(**model_kwargs).items()}
-
-
 class ArrayTranslator:
+    """
+    ArrayTranslator will convert optimizer parameter arrays into the model
+    parameters or vice versa.
+    """
     def __init__(self, graph: nx.DiGraph, translators: List[ValueTranslator],
-                 neuron_keys: Set, synapse_keys: Set, noise_keys: Set,
-                 global_keys: Set):
+                 neuron_keys: Set[str], synapse_keys: Set[str],
+                 noise_keys: Set[str], global_keys: Set[str]):
+        """
+        :param graph: The graph used for the neural circuit optimization.
+        :param translators: A list of translators with keys associated with
+        all parameters that will be optimized.
+        :param neuron_keys: A set of keys associated with NEST neuron models.
+        :param synapse_keys: A set of keys associated with NEST synapse models.
+        :param noise_keys: A set of keys associated with NEST noise models.
+        :param global_keys: A set of global keys.
+        """
         self._graph = graph
         self._translators = translators
         self._neuron_keys = neuron_keys
         self._synapse_keys = synapse_keys
         self._noise_keys = noise_keys
         self._global_keys = global_keys
+
+        self._neuron_parameters = {neuron_id: {} for neuron_id in graph.nodes()}
+        self._synapse_parameters = {}
+        self._noise_parameters = {}
+        self._global_parameters = {}
+
+        self._key_order = list(self._global_keys)\
+                          + list(self._noise_keys)\
+                          + list(self._synapse_keys)\
+                          + [key
+                             for _ in range(nx.number_of_nodes(graph))
+                             for key in self._neuron_keys]
+        self._array_size = len(self._key_order)
+        self._model_parameters = np.zeros(self._array_size)
+
+    @property
+    def synapse_parameters(self) -> Dict[str, Any]:
+        """
+        :return: A dictionary of the synaptic parameters for the NEST model.
+        """
+        return self._synapse_parameters
+
+    @property
+    def neuron_parameters(self) -> Dict[int, Dict[str, Any]]:
+        """
+        :return: A dictionary keyed by neuron id and valued by model parameters
+        for neuron models.
+        """
+        return self._neuron_parameters
+
+    @property
+    def noise_parameters(self) -> Dict[str, Any]:
+        """
+        :return: A dictionary keyed by NEST model parameter name and valued by
+        the corresponding value.
+        """
+        return self._noise_parameters
+
+    @property
+    def global_parameters(self) -> Dict[str, Any]:
+        """
+        :return: A dictionary keyed by global parameter names.
+        """
+        return self._global_parameters
+
+    def required_array_size(self) -> int:
+        """
+        :return: Size of the optimizer array required in order to satisfy the
+        parameter conversion to models.
+        """
+        return self._array_size
+
+    def from_optimizer(self, array: np.ndarray):
+        """
+        Set model parameters in-place when given an optimization array.
+        :param array: An array whose elements can be consumed by ValueTranslators.
+        :return: None
+        """
+        if len(array) != self._array_size:
+            raise ValueError("Optimizer array is wrong size. Expected {0},"
+                             " received {1}.".format(str(self._array_size),
+                                                     str(len(array))))
+        for i, opt_value in enumerate(array):
+            self._model_parameters[i] = get_translator(
+                self._translators, self._key_order[i]).to_model(opt_value)
+
+        c = 0
+        for key in self._global_keys:
+            self._global_parameters[key] = self._model_parameters[c]
+            c += 1
+        for key in self._noise_keys:
+            self._noise_parameters[key] = self._model_parameters[c]
+            c += 1
+        for key in self._synapse_keys:
+            self._synapse_parameters[key] = self._model_parameters[c]
+            c += 1
+        for neuron_pars in self._neuron_parameters.values():
+            for key in self._neuron_keys:
+                neuron_pars[key] = self._model_parameters[c]
+                c += 1
+
+    def to_optimizer(self) -> np.ndarray:
+        """
+        :return: An array consumable by an optimizer. Should not be run unless
+        from_optimizer has initialized the internal model parameters.
+        """
+        opt_array = np.zeros(self._array_size)
+        for i, key in enumerate(self._key_order):
+            opt_array[i] = get_translator(
+                self._translators, key).to_optimizer(self._model_parameters[i])
+        return opt_array
