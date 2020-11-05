3e8d87b881cd2303b8554b10cfc2f74792f4aee7
nathaniel-rodriguez <nathaniel.jm.rodriguez@gmail.com>
2020-11-05 11:51:16 -0600

    added save and load functions

--- src/neuralnetsim/data_loader.py
+++ src/neuralnetsim/data_loader.py
@@ -1,10 +1,13 @@
-__all__ = ['load_as_matrix', 'load_spike_times']
+__all__ = ['load_as_matrix', 'load_spike_times',
+           'save', 'load']
 
 
 import numpy as np
+import pickle
 from scipy.io import loadmat
 from pathlib import Path
 from typing import List
+from typing import Any
 
 
 def load_as_matrix(file_path: Path, key: str) -> np.ndarray:
@@ -34,3 +37,27 @@ def load_spike_times(file_path: Path) -> List[np.ndarray]:
     """
     raw_data = load_as_matrix(file_path, "data")
     return [raw[0] for raw in raw_data[0]]
+
+
+def save(data: Any, filename: Path, protocol=pickle.DEFAULT_PROTOCOL):
+    """
+    :param data: Save picklable Python object to file.
+    :param filename: Save file path.
+    :param protocol: Pickle protocol (default: python default).
+    :return: None
+    """
+    pickled_obj_file = open(filename, 'wb')
+    pickle.dump(data, pickled_obj_file, protocol=protocol)
+    pickled_obj_file.close()
+
+
+def load(filename: Path) -> Any:
+    """
+    Loads a pickled python object from file.
+    :param filename: Save file path.
+    :return: The loaded object.
+    """
+    pickled_obj_file = open(filename, 'rb')
+    obj = pickle.load(pickled_obj_file)
+    pickled_obj_file.close()
+    return obj
