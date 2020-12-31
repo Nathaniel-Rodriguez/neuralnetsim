4097006d54cd5c11c332c3a9257e699624123129
nathaniel-rodriguez <nathaniel.jm.rodriguez@gmail.com>
2020-12-30 18:01:35 -0600

    revised some plots

--- src/neuralnetsim/dynamical_plots.py
+++ src/neuralnetsim/dynamical_plots.py
@@ -7,15 +7,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.patches as mpatches
 import seaborn
+import pandas as pd
 from statsmodels.distributions.empirical_distribution import ECDF
 from matplotlib import cm
+import powerlaw
 from pathlib import Path
 from typing import List
 
 
 def plot_power_law_distributions(
         distributions: List[np.ndarray],
-        distribution_labels: List[str],
+        distribution_labels: List[float],
         xlabel: str = "",
         ylabel: str = "",
         save_dir: Path = None,
@@ -23,16 +25,22 @@ def plot_power_law_distributions(
         pdf: bool = True
 ):
     colors = seaborn.color_palette("flare", len(distributions))
+    fig, ax1 = plt.subplots()
     for i in range(len(distributions)):
-        ecdf = ECDF(distributions[i])
-        x = np.sort(ecdf)
-        y = 1.0 - ecdf(x)
-        plt.plot(x, y, color=colors[i], lw=2, label=distribution_labels[i])
-    plt.xscale("log")
-    plt.yscale("log")
-    plt.xlabel(xlabel)
-    plt.ylabel(ylabel)
-    plt.tight_layout()
+        x, y = neuralnetsim.eccdf(distributions[i])
+        ax1.plot(x, y, color=colors[i], lw=2,
+                 label=str(round(distribution_labels[i], 2)))
+    left, bottom, width, height = [0.1, 0.1, 0.4, 0.4]
+    ax2 = fig.add_axes([left, bottom, width, height])
+    ax2.plot(distribution_labels,
+             [powerlaw.Fit(dist).truncated_power_law.parameter1
+              for dist in distributions])
+    ax1.set_xscale("log")
+    ax1.set_yscale("log")
+    ax1.set_xlabel(xlabel)
+    ax1.set_ylabel(ylabel)
+    ax1.legend()
+    # plt.tight_layout()
     if pdf:
         plt.savefig(save_dir.joinpath(
             prefix + "_powerlaw.pdf"))
@@ -43,5 +51,10 @@ def plot_power_law_distributions(
     plt.clf()
 
 
-def plot_simulation_results(pd):
-    pass
+def plot_simulation_results(sim_df: pd.DataFrame,
+                            save_dir: Path):
+    ax = seaborn.lineplot(
+        data=sim_df,
+        x=r'$\mu$',
+        y='size'
+    )
